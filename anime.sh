c921dfef417a8f42b2aa17eaee93582896a88572
myh1000 <myh1000@gmail.com>
2016-06-19 17:17:46 -0700

    update files

--- anime.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-curl -u myh1000:J23eeelol -d data='<?xml version="1.0" encoding="UTF-8"?>
-<entry>
-	<status>1</status>
-</entry>' http://myanimelist.net/api/mangalist/add/80719.xml
--- mangahere_title.py
+++ mangahere_title.py
@@ -1,14 +1,14 @@
 import codecs
 from bs4 import BeautifulSoup
 import re
-for x in range(2,9):
+for x in range(1, 10): # change these to the number of bookmarked pages
     f = codecs.open(str(x)+".html", 'r')
     html = f.read()
     titles = re.search(b'<dt id="bp.*</dt>', html, flags=re.DOTALL).group(0)
     titles = titles.replace('\n','')
     titles = titles.replace('\t','')
     soup = BeautifulSoup(titles, 'html.parser')
-    # f = open('titles.txt', 'a')
+    f = open('titles.txt', 'a')
     for idx, link in enumerate(soup.find_all('a')):
         if (idx % 2 == 0):
             f.write(link.contents[0])
